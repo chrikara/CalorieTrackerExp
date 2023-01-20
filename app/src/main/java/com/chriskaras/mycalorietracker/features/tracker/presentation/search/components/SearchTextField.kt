65a0a1a9da44ad0832df7c68ebf1313c7a8965c1
Christos <xristoskarasoulas@gmail.com>
2023-01-20 12:54:23 +0200

    Finished whole app (Now Navigation and Testing)

---
+++ app/src/main/java/com/chriskaras/mycalorietracker/features/tracker/presentation/search/components/SearchTextField.kt
@@ -0,0 +1,107 @@
+package com.chriskaras.mycalorietracker.features.tracker.presentation.search.components
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.chriskaras.mycalorietracker.ui.theme.LocalSpacing
+
+@Composable
+fun SearchTextField(
+    text: String,
+    onValueChange: (String) -> Unit,
+    onSearch: () -> Unit,
+    modifier: Modifier = Modifier,
+    hint: String = "Search",
+    shouldShowHint: Boolean = false,
+    onFocusChanged: (FocusState) -> Unit
+) {
+    val spacing = LocalSpacing.current
+    Box(
+        modifier = modifier
+    ) {
+        BasicTextField(
+            value = text,
+            onValueChange = onValueChange,
+            singleLine = true,
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearch()
+                    defaultKeyboardAction(ImeAction.Search)
+                }
+            ),
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search,
+
+            ),
+            modifier = Modifier
+
+                .align(Alignment.CenterEnd)
+                .clip(RoundedCornerShape(5.dp))
+                .padding(2.dp)
+                .shadow(
+                    elevation = 2.dp,
+                    shape = RoundedCornerShape(5.dp)
+                )
+                .background(Color.White)
+                .fillMaxWidth()
+                .padding(spacing.spaceMedium)
+                .padding(end = 50.dp)
+                .onFocusChanged { onFocusChanged(it) }
+        ,
+            textStyle = LocalTextStyle.current.copy(
+                textAlign = TextAlign.Start
+            )
+
+        )
+        if(shouldShowHint) {
+            Text(
+                text = hint,
+                style = MaterialTheme.typography.bodyMedium,
+                fontWeight = FontWeight.Light,
+                color = Color.LightGray,
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .padding(start = spacing.spaceMedium)
+            )
+        }
+        IconButton(
+            onClick = onSearch,
+            modifier = Modifier.align(Alignment.CenterEnd)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = ""
+            )
+        }
+    }
+}
